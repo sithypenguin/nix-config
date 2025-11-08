957d4ae543c4d83f8cec550b36a03a7e78d40a84
Will Edwards <sithypenguin@gmail.com>
2025-11-08 12:59:34 -0500

    Create laptop profile

---
+++ profiles/laptop.nix
@@ -0,0 +1,11 @@
+{ ... }:
+
+{
+    laptop = {
+        laptop.enable = true;
+        desktop.environment = "plasma6";
+        gaming.enable = true;
+        gaming.steal = true;
+        development.enable = true;
+    }
+}
