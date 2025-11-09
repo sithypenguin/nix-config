c0bee4c1750f3acf33a2829c6a9438059e2982a8
Will Edwards <sithypenguin@gmail.com>
2025-11-08 20:11:20 -0500

    Enable networkmanager at the top level

--- configuration.nix
+++ configuration.nix
@@ -15,6 +15,8 @@
     ./general/steam.nix
   ];
 
+  networking.networkmanager.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
