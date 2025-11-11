caebd841d457ea0a5d8802f511f4f1b4be6b1b7a
Will Edwards <sithypenguin@gmail.com>
2025-11-10 19:19:01 -0500

    Remove ./packages from system level config

--- modules/default.nix
+++ modules/default.nix
@@ -9,9 +9,6 @@
         ./systemConfig/host-options.nix
         ./systemConfig/networking.nix
         ./systemConfig/sysConfig.nix
-        ./packages/gui-packages.nix
-        ./packages/sys-util-packages.nix
-        ./packages/tui-packages.nix
         ./gaming/steam.nix
         ./users/users.nix
     ];
