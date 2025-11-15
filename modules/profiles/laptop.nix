d27c1a6087c757d54845187c57c59dc81fa0c7f1
Will Edwards <sithypenguin@gmail.com>
2025-11-15 18:26:40 -0500

    Change back to plasma6

--- modules/profiles/laptop.nix
+++ modules/profiles/laptop.nix
@@ -3,8 +3,8 @@
 {
     mySystem = {
         laptop.enable = true;
-        #laptop.environment = "plasma6";
-        laptop.environment = "hyprland";
+        laptop.environment = "plasma6";
+        #laptop.environment = "hyprland";
         gaming.enable = true;
         gaming.steam = true;
         development.enable = true;
