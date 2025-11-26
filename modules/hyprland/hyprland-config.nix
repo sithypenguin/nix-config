e5ce7a1877befc15788b3229d62eb901d7c12509
Will Edwards <sithypenguin@gmail.com>
2025-11-26 10:05:40 -0500

    Move all hyprland config files to git repo

---
+++ modules/hyprland/hyprland-config.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, ... }:
+
+{
+  # Hyprland configuration files
+  home.file.".config/hypr/hyprland.conf".source = ../../dotfiles/hyprland/hypr/hyprland.conf;
+  home.file.".config/hypr/hyprpaper.conf".source = ../../dotfiles/hyprland/hypr/hyprpaper.conf;
+  
+  # Waybar configuration files
+  home.file.".config/waybar/config.json".source = ../../dotfiles/hyprland/waybar/config.json;
+  home.file.".config/waybar/style.css".source = ../../dotfiles/hyprland/waybar/style.css;
+  
+  # Mako configuration
+  home.file.".config/mako/config".source = ../../dotfiles/hyprland/mako/config;
+  
+  # Rofi configuration
+  home.file.".config/rofi/config.rasi".source = ../../dotfiles/hyprland/rofi/config.rasi;
+}
